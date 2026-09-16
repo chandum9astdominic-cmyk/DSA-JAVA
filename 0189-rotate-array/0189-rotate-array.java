a1a820562a87d6f095d701924c0b5a11064b6a0f
CHANDU -M010 <chandum9a.stdominic@gmail.com>
2026-09-16 06:07:27 +0530

    Time: 4 ms (51.96%), Space: 266.2 MB (48.63%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+
+        k = k % n;
+
+        int[] temp = new int[k];
+
+        // Store last k elements
+        for(int i = 0; i < k; i++) {
+            temp[i] = nums[n-k+i];
+        }
+
+        // Shift remaining elements
+        for(int i = n-k-1; i >= 0; i--) {
+            nums[i+k] = nums[i];
+        }
+
+        // Put stored elements at beginning
+        for(int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
