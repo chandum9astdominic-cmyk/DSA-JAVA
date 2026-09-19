649c2bcafa325be342b6a22dc028070b682ea8ab
CHANDU -M010 <chandum9a.stdominic@gmail.com>
2026-09-19 21:37:34 +0530

    Time: 1 ms (99.96%), Space: 42.3 MB (90.37%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        while(x!=0)
+         {
+             if (ans > Integer.MAX_VALUE / 10 ||
+                ans < Integer.MIN_VALUE / 10) 
+        {
+            return 0;
+        }
+           int  lastdigit = x%10;
+           ans = ans *10+lastdigit;
+            x=x/10;
+         } 
+         return ans;
+    }
+}
