84cc1e64d227f8067721ec25dedc404114c44ba4
CHANDU -M010 <chandum9a.stdominic@gmail.com>
2026-09-20 19:22:06 +0530

    Time: 7 ms (13.08%), Space: 44.6 MB (88.03%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        int k =0;
+        for(int i =0;i<nums1.length;i++)
+        {
+            for(int j=0;j<nums2.length;j++)
+            {
+                if(nums1[i]==nums2[j])
+                {
+                    set.add(nums1[i]);
+                }
+            }
+        }
+        int [] result = new int[set.size()];
+        for (int value : set) {
+           result[k] = value;
+            k++;
+         }
+        return result;
+    }
+}
